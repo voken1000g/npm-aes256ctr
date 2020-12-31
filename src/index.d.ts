ab7f51c2efacf59d013604444e29a2add8eff4d4
Lyman Snow <pmvoken@protonmail.com>
2020-12-31 20:02:39 +0000

    updated: ts

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="node" />
+export declare function encrypt(bufData: Buffer, bufSecret: Buffer): Buffer;
+export declare function decrypt(bufData: Buffer, bufSecret: Buffer): Buffer;
--- src/index.js
+++ src/index.js
@@ -12,24 +12,32 @@ const ALGORITHM = 'aes-256-ctr'
 // chacha20-poly1305
 
 // https://www.geeksforgeeks.org/node-js-crypto-createcipheriv-method
-function encrypt(bufData, secret) {
-  const bufSecret = Buffer.isBuffer(secret) ? secret : Buffer.from(secret)
-  const bufHash = sha.sha384(bufSecret)
+function encrypt(data, secret) {
+  if (Array.isArray(data) || data instanceof Uint8Array) { data = Buffer.from(data) }
+  if (!Buffer.isBuffer(data)) { throw new TypeError('Expected Buffer') }
+  if (Array.isArray(secret) || secret instanceof Uint8Array) { secret = Buffer.from(secret) }
+  if (!Buffer.isBuffer(secret)) { throw new TypeError('Expected Buffer') }
+
+  const bufHash = sha.sha384(secret)
   const cipher = crypto.createCipheriv(ALGORITHM, bufHash.slice(0, 32), bufHash.slice(32, 48));
-  const encrypted = cipher.update(bufData);
+  const encrypted = cipher.update(data);
   return Buffer.concat([encrypted, cipher.final()]);
 }
 
 // https://www.geeksforgeeks.org/node-js-crypto-createdecipheriv-method
-function decrypt(bufData, secret) {
-  const bufSecret = Buffer.isBuffer(secret) ? secret : Buffer.from(secret)
-  const bufHash = sha.sha384(bufSecret)
+function decrypt(data, secret) {
+  if (Array.isArray(data) || data instanceof Uint8Array) { data = Buffer.from(data) }
+  if (!Buffer.isBuffer(data)) { throw new TypeError('Expected Buffer') }
+  if (Array.isArray(secret) || secret instanceof Uint8Array) { secret = Buffer.from(secret) }
+  if (!Buffer.isBuffer(secret)) { throw new TypeError('Expected Buffer') }
+
+  const bufHash = sha.sha384(secret)
   const decipher = crypto.createDecipheriv(ALGORITHM, bufHash.slice(0, 32), bufHash.slice(32, 48));
-  const decrypted = decipher.update(bufData);
+  const decrypted = decipher.update(data);
   return Buffer.concat([decrypted, decipher.final()]);
 }
 
 module.exports = {
   encrypt: encrypt,
-  decrypt: decrypt
+  decrypt: decrypt,
 }
